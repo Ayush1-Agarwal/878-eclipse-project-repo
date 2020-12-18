c42bcac6ed6df079c8e7019c4908c9a1864b548b
Ayush Agarwal <agayush456@gmail.com>
2020-12-18 12:21:21 +0530

    here error will also come

--- gitcheckproject/src/gitcheckproject/GitClass.java
+++ gitcheckproject/src/gitcheckproject/GitClass.java
@@ -3,7 +3,7 @@ package gitcheckproject;
 public class GitClass {
 
 	public static void main(String[] args) {
-		System.out.println("hello git");
+		System.out.println("here error willn also come");
 		System.out.println("changing and checking again");
 
 		System.out.println("here error should come");
