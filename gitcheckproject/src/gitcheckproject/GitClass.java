dc4cfbb6cb6975856900e8c1b43fbe9e72532eec
Ayush Agarwal <agayush456@gmail.com>
2020-12-18 12:20:18 +0530

    again checking conflict

--- gitcheckproject/src/gitcheckproject/GitClass.java
+++ gitcheckproject/src/gitcheckproject/GitClass.java
@@ -3,11 +3,11 @@ package gitcheckproject;
 public class GitClass {
 
 	public static void main(String[] args) {
-		System.out.println("hello git");
+		System.out.println("again checking conflict");
 		System.out.println("changing and checking again");
 
 		System.out.println("here error should come");
-
+        
 	}
 
 }
